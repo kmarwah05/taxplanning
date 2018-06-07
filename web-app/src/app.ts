b23efb74f51abd0b33fd1548f5bc9a75c8281ea1
Karandeep Marwah <kmarwah@pietech.com>
2018-06-07 17:21:16 -0400

    Asset Table

--- web-app/src/app.ts
+++ web-app/src/app.ts
@@ -1,3 +1,18 @@
 export class App {
-  message = 'Hello World!';
+
+counter:number= 0;
+ 
+  addButton(){
+   var assetTable: string = "<tr><td><input type=\"text\" id=\"AssetName"+this.counter+"\" class=\"form-control\"/></td>"
+
+   assetTable += 
+
+   "<td><input type=\"text\" id=\"AssetType"+this.counter+"\"class=\"form-control\"/></td>"+
+   "<td><input type=\"text\" id=\"AssetValue"+this.counter+"\" class=\"form-control\" /></td>"+
+   "</tr>"
+
+   document.getElementById("assetIdTable").innerHTML+=assetTable;
+   this.counter++;
+  }
+
 }
