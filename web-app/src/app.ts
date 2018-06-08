e47e25a599820fe6277d0ca4ddc9484d26ea1ae7
Austin Sarmento <asarmento@pietech.com>
2018-06-08 14:03:54 -0400

    Got routing working

--- web-app/src/app.ts
+++ web-app/src/app.ts
@@ -1,12 +1,12 @@
 import {RouterConfiguration, Router} from 'aurelia-router';
-
+import {PLATFORM} from 'aurelia-pal';
 export class App {
   router:Router;
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'Tax Planing';
     config.map([
-      {route: ['', 'home'], name: 'home', moduleId: 'Home/home', nav: true, title: 'Home'},
-      {route: 'results', name: 'results', moduleId: 'Results/results', nav: true, title: 'Results'}
+      {route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('home/home'), nav: true, title: 'Home'},
+      {route: 'results', name: 'results', moduleId:  PLATFORM.moduleName('results/results'), nav: true, title: 'Results'}
     ]);
     this.router = router;
   }
