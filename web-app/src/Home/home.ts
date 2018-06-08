cb5022710f5a40c56e49c6e47797afb5e1746353
Austin Sarmento <asarmento@pietech.com>
2018-06-08 13:41:37 -0400

    Restructured folders

---
+++ web-app/src/Home/home.ts
@@ -0,0 +1,26 @@
+export class Home{
+counter:number= 0;
+ 
+  addButton(){
+   var assetTable: string = "<tr><td><input type=\"text\" id=\"AssetName"+this.counter+"\" class=\"form-control\"/></td>"
+
+   assetTable += 
+   
+
+   "<td><input type=\"text\" id=\"AssetType"+this.counter+"\"class=\"form-control\"/></td>"+
+   "<td><input type=\"text\" id=\"AssetValue"+this.counter+"\" class=\"form-control\" /></td>"+
+   "<td style=\"width:10px\"><button click.delegate=\"removeButton()\"><span class=\"glyphicon glyphicon-minus addBtn\"></span></td>"+
+   "</tr>"
+
+   document.getElementById("assetIdTable").innerHTML += (assetTable);
+   this.counter++;
+  }
+
+
+
+  removeButton(){
+    var assetTable2: string = "<tr><td><input type=\"text\" id=\"AssetName"+this.counter+"\" class=\"form-control\"/></td>"
+
+    "<td><input type=\"text\" id=\"AssetType"+this.counter+"\"class=\"form-control\"/></td>"
+  }
+}
