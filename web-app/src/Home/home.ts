c5d20a619aa3e3766423bd11934c0e789dfa03ae
Austin Sarmento <asarmento@pietech.com>
2018-06-11 16:21:33 -0400

    wow, the table works

--- web-app/src/Home/home.ts
+++ web-app/src/Home/home.ts
@@ -1,43 +1,34 @@
-export class Home{
-counter:number= 0;
-assets = [];
-name: string
-type: string
-value: string
+export class Home {
+  counter: number = 0;
+  assets = [{ "name": "blaj", "type": "401K", "value": "this.value" }];
+  name: string
+  type: string
+  value: string
 
-  regenTable(){
-   var assetTable: string = "<tr>"+
-   "<td><input type=\"text\" class=\"form-control\" id=\"Aname\" value.bind=\"name\"></td>"+
-   "<td><select class=\"form-control h-100\" id=\"Atype\" value.bind=\"type\">"+
-       "<option>Choose Asset Type</option>"+
-       "<option value=\"Ira\">IRA</option>"+
-       "<option value=\"RothIra\">Roth IRA</option>"+
-       "<option value=\"401K\">401K</option>"+
-   "</select></td>"+
-   "<td><input type=\"text\" class=\"form-control\" id=\"Avalue\" value.bind=\"value\"></td>"+
-   `<th><button click.delegate=\"addButton()\"><span class=\"glyphicon glyphicon-plus addBtn\"></th>`+
- "</tr>"+
- "<tr repeat.for=\"assets of $displayData\">"+
-   `<td>${this.assets[this.counter].name}</td>`+
-   `<td>${this.assets[this.counter].type}</td>`+
-   `<td>${this.assets[this.counter].value}</td>`+
- "</tr>"
+  regenTable() {
+    var assetTable: string =
+      "<tr>" +
+      `<td>${this.assets[this.counter].name}</td>` +
+      `<td>${this.assets[this.counter].type}</td>` +
+      `<td>${this.assets[this.counter].value}</td>` +
+      "</tr>"
 
-   document.getElementById("AssetTable").innerHTML += "";
-   this.counter++;
-  }
-
-  addButton(){
-    let asset = {"name":this.name,"type":this.type, "value":this.value}
-    JSON.stringify(asset)
-    this.assets.push(asset)
-    //console.log("Binding "+this.assets);
-    this.regenTable();
+    document.getElementById("AssetTable").innerHTML += assetTable;
     this.counter++;
   }
 
-  bind(){
-    console.log("Binding "+this.assets);
+  addButton() {
+    let asset = { "name": this.name, "type": this.type, "value": this.value }
+    //JSON.stringify(asset)
+    //this.assets.push(asset)
+    this.assets = [...this.assets, asset]
+    console.log("adding ", this.assets);
+    //console.log("Binding "+this.assets);
+    //this.regenTable();
+  }
+
+  bind() {
+    console.log("Binding " + this.assets);
     return this.assets
   }
 }
