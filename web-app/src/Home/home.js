231fe6e5b343322bae8becf73c2d6be4f03adeaf
Karandeep Marwah <kmarwah@pietech.com>
2018-06-11 13:11:16 -0400

    home page css styles

---
+++ web-app/src/Home/home.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.addBtn').on('click', function(){
+    addTableRow();
+  });
+  $('.addBtnRemove').click(function(){
+    $(this).closest('tr').remove();
+  })
+
+  var i = 1;
+  function addTableRow(){
+    var tempTr = $('<tr><td><input type="text" id="userName_' + i + 
+    '" class="form-control"/></td><td><input type="text" id="email_' + i + 
+    '" class="form-control" /></td><td><input type="text" id="password_' + i + 
+    '" class="form-control" /></td><td><span class="glyphicon glyphicon-minus addBtnRemove" id="addBtn_' + i + 
+    '"></span></td></tr>').on('click', function () {
+      $(this).closest('tr').remove(); 
+      $(document.body).on('click', '.TreatmentHistoryRemove', function (e) {
+           $(this).closest('tr').remove();  
+       });
+   });
+   $("#tableAddRow").append(tempTr)
+   i++;
+}
+});
--- web-app/src/Home/home.ts
+++ web-app/src/Home/home.ts
@@ -1,6 +1,7 @@
 export class Home{
 counter:number= 0;
  
+/*
   addButton(){
    var assetTable: string = "<tr><td><input type=\"text\" id=\"AssetName"+this.counter+"\" class=\"form-control\"/></td>"
 
@@ -8,19 +9,13 @@ counter:number= 0;
    
 
    "<td><input type=\"text\" id=\"AssetType"+this.counter+"\"class=\"form-control\"/></td>"+
-   "<td><input type=\"text\" id=\"AssetValue"+this.counter+"\" class=\"form-control\" /></td>"+
+  "<td><input type=\"text\" id=\"AssetValue"+this.counter+"\" class=\"form-control\" /></td>"+
    "<td style=\"width:10px\"><button click.delegate=\"removeButton()\"><span class=\"glyphicon glyphicon-minus addBtn\"></span></td>"+
    "</tr>"
 
    document.getElementById("assetIdTable").innerHTML += (assetTable);
    this.counter++;
   }
+  */
 
-
-
-  removeButton(){
-    var assetTable2: string = "<tr><td><input type=\"text\" id=\"AssetName"+this.counter+"\" class=\"form-control\"/></td>"
-
-    "<td><input type=\"text\" id=\"AssetType"+this.counter+"\"class=\"form-control\"/></td>"
-  }
 }
