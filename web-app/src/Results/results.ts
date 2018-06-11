dc36e1c315ad468077496674d2d1e6cd9bdff261
Austin Sarmento <asarmento@pietech.com>
2018-06-11 11:48:54 -0400

    Comunication with api works with no cors

--- web-app/src/Results/results.ts
+++ web-app/src/Results/results.ts
@@ -3,9 +3,10 @@ import {HttpClient} from 'aurelia-fetch-client';
 export class Results{
   yearStart: number
   yearEnd: number
+  client: HttpClient
+  endpoint: string = 'http://localhost:60419/api'
   
   CreateTable() {
-    const endpointL: string = "kraken:" //update with api endpoint
     var tableContent: string = "";
     for(var i = this.yearStart; i < this.yearEnd; i++){
       tableContent += "<tr>";
@@ -14,4 +15,46 @@ export class Results{
       tableContent += "</tr>";
     }
   }
+
+  ConfigureClient(){
+    let client = new HttpClient;
+    client.configure(config => {
+      config
+        .withBaseUrl(this.endpoint)
+          .withDefaults({
+            mode:'cors',
+            headers: {
+              'content-type': 'multipart/form-data',
+              'Accept': 'application/json'
+            }
+          })
+    });
+    this.client = client;
+  }
+
+  GetResults(){
+    this.ConfigureClient();
+
+    let form = new FormData;
+    form.set('','');
+    form.set('','');
+    form.set('','');
+    form.set('','');
+    form.set('','');
+    form.set('','');
+    form.set('','');
+    form.set('','');
+
+    this.SendPost(form);
+  }
+
+  SendPost(form){
+    this.client.fetch(this.endpoint,{
+      method:"POST",
+      body: form
+    })
+    .then(response => response.json())
+    .then(data => {console.log(data)});
+  }
+
 }
