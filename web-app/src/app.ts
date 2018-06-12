71eb21382124d894ff6951bc53df680fe08b25ae
Karandeep Marwah <kmarwah@pietech.com>
2018-06-12 11:28:21 -0400

    new changes to the home page

--- web-app/src/app.ts
+++ web-app/src/app.ts
@@ -3,7 +3,7 @@ import {PLATFORM} from 'aurelia-pal';
 export class App {
   router:Router;
   configureRouter(config, router) {
-    config.title = 'Tax Planing';
+    config.title = 'Tax Planning';
     config.options.pushState = true;
     config.map([
       {route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('home/home'), nav: true, title: 'Home'},
