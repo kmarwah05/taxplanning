7066d3c9476aa82ced78bc3aa84065f57e5e07dd
Austin Sarmento <asarmento@pietech.com>
2018-06-12 11:31:45 -0400

    working on results page

--- web-app/src/Home/home.ts
+++ web-app/src/Home/home.ts
@@ -4,23 +4,37 @@ export class Home {
   name: string
   type: string
   value: string
+  filingStatus: string
+  income: string
 
   addButton() {
+    //create a new asset and add it to the assets array
     let asset = { "name": this.name, "type": this.type, "value": this.value, "id":this.counter }
     this.assets = [...this.assets, asset]
     this.counter++;
-    console.log("adding ", this.assets);
+    //console.log("adding ", this.assets);
+    this.jsonify() //save data when a change is made
   }
 
   removeButton(id){
     let i = 0;
-    console.log("Removing",id);
-    this.assets.splice(id,1);
-    this.assets.forEach(element => {
+    //console.log("Removing",id);
+    this.assets.splice(id,1);         //remove the element that you clicked
+    this.assets.forEach(element => {  //fix the array index
       element.id = i
       i++;
     });
     this.counter--;
+    this.jsonify() //save data when a change is made
+  }
+
+  //stores data so that it can be used on the results page
+  jsonify(){
+    sessionStorage.userData = JSON.stringify(
+      {
+      "FilingStatus":this.filingStatus,
+      "Income":this.income,
+      "Assets":this.assets});
   }
 
   bind() {
---
+++ web-app/src/HttpService.ts
@@ -0,0 +1,31 @@
+import {HttpClient} from 'aurelia-fetch-client';
+
+export class HttpService{
+  client: HttpClient
+  endpoint: string = 'http://localhost:60419/api'
+  
+  ConfigureClient(){
+    let client = new HttpClient;
+    client.configure(config => {
+      config
+        .withBaseUrl(this.endpoint)
+          .withDefaults({
+            mode:'no-cors',
+            headers: {
+              'content-type': 'mulipart/form-data',
+              'Accept': 'application/json'
+            }
+          })
+    });
+    this.client = client;
+  }
+  
+  SendPost(form){
+    this.client.fetch(this.endpoint,{
+      method:"POST",
+      body: form
+    })
+    .then(response => response.json())
+    .then(data => {console.log(data)});
+  }
+}
