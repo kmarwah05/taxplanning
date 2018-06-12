fb85f2c892504bf2998d0e1cdf664477e864f62a
Austin Sarmento <asarmento@pietech.com>
2018-06-12 14:37:54 -0400

    passing off results for css work

--- web-app/src/Results/results.ts
+++ web-app/src/Results/results.ts
@@ -1,42 +1,42 @@
-import {HttpClient} from 'aurelia-fetch-client';
 import { inject } from 'aurelia-framework';
 import { HttpService } from 'HttpService';
 
 @inject(HttpService)
-export class Results{
-  
-  constructor(private httpService: HttpService){
+export class Results {
+  total: number = 4325;
+  net: number = 2834
+
+  constructor(private httpService: HttpService) {
     httpService.ConfigureClient();
   }
- 
+
   CreateTable() {
   }
-  
-  GetResults(){
+
+  GetResults() {
     let form = new FormData;
     var data = JSON.parse(sessionStorage.userData)
     //console.log(data)
 
-    form.set('FilingStatus',data.FilingStatus);
-    form.set('Income',data.Income);
-    form.set('BasicAdjustment','1000');
-    form.set('RetirementDate','423');
-    form.set('EndOfPlanDate','15332');
-    form.set('CapitalGains','423');
-    form.set('FormAssets',this.BuildAssetString(data.Assets));
+    form.set('FilingStatus', data.FilingStatus);
+    form.set('Income', data.Income);
+    form.set('BasicAdjustment', '1000');
+    form.set('RetirementDate', '423');
+    form.set('EndOfPlanDate', '15332');
+    form.set('CapitalGains', '423');
+    form.set('FormAssets', this.BuildAssetString(data.Assets));
 
     this.httpService.SendPost(form);
   }
 
-  BuildAssetString(array)
-  {
+  BuildAssetString(array) {
     var assetString: string = "["
 
-    for(let i = 0; i < array.length; i++){
-      assetString += ("[\""+array[i].name+"\",\""+array[i].type+"\",\""+array[i].value+"\"]");
-      
-      if((i+1) <= array.length){ //while you have more assets add a comma
-        assetString+=","
+    for (let i = 0; i < array.length; i++) {
+      assetString += ("[\"" + array[i].name + "\",\"" + array[i].type + "\",\"" + array[i].value + "\"]");
+
+      if ((i + 1) <= array.length) { //while you have more assets add a comma
+        assetString += ","
       }
     }
 
