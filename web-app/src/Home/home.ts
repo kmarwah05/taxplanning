f4f6637d7ad1022286cf84798e85f47a78366ce2
Karandeep Marwah <kmarwah@pietech.com>
2018-06-18 14:02:49 -0400

    Range Slider done!!!

--- web-app/src/Home/home.ts
+++ web-app/src/Home/home.ts
@@ -1,9 +1,13 @@
+import { Home } from './home';
 import { inject, NewInstance } from 'aurelia-framework';
 import { ValidationRules, ValidationController } from 'aurelia-validation';
 import $ from '../../node_modules/jquery/dist/jquery.js';
+import 'aurelia-ion-rangeslider';
+;
 
 
 @inject(NewInstance.of(ValidationController))
+
 
 
 export class Home {
@@ -15,8 +19,8 @@ export class Home {
   filingStatus: string
   income: string
   basicAdjustment: string = '';
-  retirementDate: string
-  endOfPlan: string
+  retirementDate: string;
+  endOfPlan: string;
   capitalGains: string
   message = '';
   errors = []
@@ -33,6 +37,7 @@ export class Home {
     this.name = ''
     this.type = ''
     this.value = ''
+    this.jsonify()
   }
 
   removeButton(id) {
@@ -48,10 +53,13 @@ export class Home {
 
   //stores data so that it can be used on the results page
   jsonify() {
-    if(this.name.length != 0 && this.type.length !=0 && this.value.length !=0)
-    {
-      this.addButton()
-    }
+    // if (this.name.length != 0 && this.type.length != 0 && this.value.length != 0) {
+    //   this.addButton()
+    // }
+    var t = document.getElementById("test").innerText.split(",")
+    this.retirementDate = t[0]
+    this.endOfPlan = t[1]
+    console.log("T: ",t," RD: ",this.retirementDate," EOP: ",this.endOfPlan)
     sessionStorage.userData = JSON.stringify(
       {
         "filingStatus": this.filingStatus,
@@ -64,23 +72,33 @@ export class Home {
         "desiredAdditions": this.desiredAdditions,
         "desiredWithdrawals": this.desiredWithdrawls
       });
+      console.log(sessionStorage.userData = JSON.stringify(
+        {
+          "filingStatus": this.filingStatus,
+          "income": this.incomeValidate,
+          "basicAdjustment": this.basicAdjustment,
+          "retirementDate": this.retirementDate,
+          "endOfPlanDate": this.endOfPlan,
+          "capitalGains": this.capitalGains,
+          "assets": this.assets,
+          "desiredAdditions": this.desiredAdditions,
+          "desiredWithdrawals": this.desiredWithdrawls
+        }))
   }
 
   bind() {
     return this.assets
   }
-  
+
 
   constructor(private controller: ValidationController) {
     ValidationRules
       .ensure((m: Home) => m.filingStatus).displayName("Filing Status").required()
       .ensure((m: Home) => m.incomeValidate).displayName("Income value").required().matches(new RegExp(/[0-9]/))
       .ensure((m: Home) => m.basicAdjustment).displayName("Basic Adjustment value").required().matches(new RegExp(/[0-9]/))
-      .ensure((m: Home) => m.retirementDate).displayName("Retirement Date in yyyy").required().matches(new RegExp(/^[0-9]{4}$/))
       .ensure((m: Home) => m.capitalGains).displayName("Capital gains value").required().matches(new RegExp(/[0-9]/))
-      .ensure((m: Home) => m.endOfPlan).displayName("End of Plan in yyyy").required().matches(new RegExp(/^[0-9]{4}$/))
-      .ensure((m: Home) => m.desiredAdditions).displayName("Desired Additions").required().matches(new RegExp(/[0-9]/))
-      .ensure((m: Home) => m.desiredWithdrawls).displayName("Desired Withdrawls").required().matches(new RegExp(/[0-9]/))
+      .ensure((m: Home) => m.desiredAdditions).displayName("Additions").required().matches(new RegExp(/[0-9]/))
+      .ensure((m: Home) => m.retirementDate).displayName("Retirement Date and End of Plan").required()
       .on(this);
   }
 
@@ -97,19 +115,51 @@ export class Home {
       })
   }
 
-/*
-minYear = new Date().getFullYear()
-maxYear = this.minYear + 80
 
-myMin = this.minYear;
-myMax = this.maxYear;
-*/
 
-// sliders starts here
+  // sliders starts here
 
   attached() {
-  
+    var from = new Date().getFullYear()
+    var to = this.endOfPlan
+
+    var date = new Date().getFullYear()
+    $(function () {
+      $('#range').ionRangeSlider({
+        min: date,
+        max: date + 80,
+        from: from,
+        to: to,
+        type: 'int',
+        grid: true,
+        grid_num: 10,
+        onChange: function (data){
+          $('#test').prop("innerText",[data.from, data.to])
+        },
+        onStart: function (data){
+          $('#test').prop("innerText",[data.from, data.to])
+        }
+      });
+    });
+
   }
-// sliders ends here
+
+
+  // sliders ends here
 
 }
+
+ /*
+    $('#range').on("change", function () {
+      var $this = $(this),
+        value = $this.prop("value").split(";");
+      this.retirementDate = value[0]
+      console.log("ON FINISH:", this.retirementDate)
+      this.endOfPlan = value[1] 
+      this.save(value[0],value[1])
+    })
+    
+     save(from , to){
+    this.retirementDate = from;
+    this.endOfPlan = to;
+    */
