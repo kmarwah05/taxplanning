9342e5a97c07f0d709e801aff6bfdad66d753d0c
Austin Sarmento <asarmento@pietech.com>
2018-06-21 09:24:20 -0400

    data saves when you go back. small validiation improvements

--- web-app/src/Home/home.ts
+++ web-app/src/Home/home.ts
@@ -56,6 +56,9 @@ export class Home {
     if (this.name.length != 0 && this.type.length != 0 && this.value.length != 0) {
       this.addButton()
     }
+    if(this.age != undefined){
+      this.addChildren()
+    }
     var arr = this.tChildren.map(x => parseInt(x.age))
     sessionStorage.userData = JSON.stringify(
       {
@@ -74,7 +77,26 @@ export class Home {
     return this.assets
   }
 
+  MakeChildArr(ages) {
+    for (var i = 0, childArr = new Array(ages.length); i < ages.length;) {
+      childArr[i] = { "age": ages[i], "id": i };
+      i++;
+    }
+    return childArr;
+  }
+
   constructor(private controller: ValidationController) {
+    if (sessionStorage.getItem("userData")) {
+      var storage = JSON.parse(sessionStorage.userData);
+      this.assets = storage.assets
+      this.filingStatus = storage.filingStatus
+      this.income = storage.income
+      this.retirementDate = storage.retirementDate
+      this.endOfPlan = storage.endOfPlanDate
+      this.desiredAdditions = storage.desiredAdditions
+      this.tChildren = this.MakeChildArr(storage.childrensAges)
+      this.currentAge = storage.currentAge
+    }
     ValidationRules.customRule(
       'integerRange',
       (value, obj, min, max) => {
@@ -87,8 +109,8 @@ export class Home {
 
     ValidationRules
       .ensure((m: Home) => m.filingStatus).displayName("Filing Status").required()
-      .ensure((m: Home) => m.income).displayName("Income value").required().matches(new RegExp(/^\d*[0-9]\d*$/))
-      .ensure((m: Home) => m.desiredAdditions).displayName("Additions").required().matches(new RegExp(/^\d*[0-9]\d*$/))
+      .ensure((m: Home) => m.income).displayName("Income value").required().satisfiesRule('integerRange',0,1000000000000)
+      .ensure((m: Home) => m.desiredAdditions).displayName("Additions").required().satisfiesRule('integerRange',0,1000000000000)
       .ensure((m: Home) => m.currentAge).displayName("Current Age").required().satisfiesRule('integerRange', 1, 200)
       .on(this);
   }
@@ -122,8 +144,8 @@ export class Home {
     var statusSelector: HTMLSelectElement;
     var incomeText = document.getElementById("incomeRange")
     var self = this
-    this.retirementDate = from+20+''
-    this.endOfPlan = to-30+''
+    this.retirementDate = from + 20 + ''
+    this.endOfPlan = to - 30 + ''
 
     for (let i = from; i <= to; i += 10) {
       dates.push(i) //get all the years from now for 80 years
@@ -131,7 +153,7 @@ export class Home {
 
     //create slider
     noUiSlider.create(range, {
-      start: [from+20, to-30],
+      start: [from + 20, to - 30],
       range: {
         min: from,
         max: to
@@ -191,6 +213,7 @@ export class Home {
       var child = { "age": this.age, "id": this.childId }
       this.tChildren = [...this.tChildren, child]
       this.childId++;
+      this.age = undefined
     }
   }
 
